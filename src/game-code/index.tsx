76926c9e5f3f4fd45435ec14a1b98861c342ab8e
Ray <dev@hiray.me>
2021-05-02 11:01:39 +0100

    Fixed scaling issues and made audio loop

--- src/game-code/index.tsx
+++ src/game-code/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 //import music from './assets/Canned_Warhol_Mix_1.mp3';
@@ -9,15 +9,34 @@ import * as Pixi from 'pixi.js';
 
 import main from './main';
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+}
+
 const game = () => {
     const app = new Pixi.Application({ antialias: true, backgroundColor: 0xf2ecea });
 
     const audioTune = new Audio('/assets/Canned_Warhol_Mix_1.mp3');
 
+    const [width, height] = useWindowSize();
+    useEffect(() => {
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+    }, [width, height]);
+
     // Logic here
     useEffect(() => {
         main(app);
         app.renderer.resize(window.innerWidth, window.innerHeight);
+        audioTune.loop = true;
         audioTune.load();
         document.getElementById('root')?.appendChild(app.view);
     }, []);
